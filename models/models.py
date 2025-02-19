614b0aedbedbfc73b068ad75c68ccd1f30cc9f33
leticiaOntiveros <leticiaontiveros41@gmail.com>
2025-02-19 15:50:36 -0600

    ya se hizo el cud dinamico en la rama de logistica, pero ahora falta el metodo del update

---
+++ models/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+
+# Configura la conexión a la base de datos
+DATABASE_URL = "mysql+pymysql://logisticaRODVAL:logisticaRODVAL@localhost/rodval"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Metadata para reflejar las tablas existentes
+metadata = MetaData()
+metadata.reflect(bind=engine)
+
+# Función para obtener una sesión de la base de datos
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
